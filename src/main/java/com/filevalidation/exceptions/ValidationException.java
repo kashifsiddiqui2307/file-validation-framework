5355eeed5450bf359ff0fe7de5d219693d1feca1
kashifsiddiqui2307 <kashif.siddiqui0723@gmail.com>
2026-07-21 16:00:34 +0200

    Add custom ValidationException class

---
+++ src/main/java/com/filevalidation/exceptions/ValidationException.java
@@ -0,0 +1,16 @@
+package com.filevalidation.exceptions;
+
+/**
+ * Custom exception for validation framework
+ */
+public class ValidationException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
